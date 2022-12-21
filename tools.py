f8b8127c1f76b119a4e701ca121600470affdcc0
Hugo Neveux <hugoneveux@protonmail.com>
2022-12-21 22:18:14 +0100

    Move the innocent_suspect function to tools.py

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+import datetime
 import tweepy 
 import graphh 
 import json
@@ -5,7 +6,7 @@ import requests
 import csv
 
 from suspect import Suspect
-from constants import PHONE_OPERATORS_URL, SUSPECTS_COLORS
+from constants import CRIME_DATE, CRIME_LOCATION, PHONE_OPERATORS_URL, SUSPECTS_COLORS
 from cartography import *
 
 def api_init() -> tuple[tweepy.Client, graphh.GraphHopper]:
@@ -57,3 +58,31 @@ def get_provider_data() -> dict:
     each suspect's phone. 
     """
     return requests.get(PHONE_OPERATORS_URL).json()
+
+def innocent_suspect(ghclient, dict_loc: dict, suspect):
+    """ 
+    Innocents the suspect if they can't make it beetween their location and
+    the crime scene 
+    """
+    time_format = "%Hh and %M minutes" 
+    location = [dict_loc['lat'], dict_loc['long']]
+    time = dict_loc['date']
+
+    available_time = abs(CRIME_DATE - time)
+
+    duration = ghclient.duration([location, CRIME_LOCATION], vehicle='car', unit='min')
+    duration_to_crime_scene = datetime.timedelta(minutes=duration)
+    
+    # Taking the phone location delta (2 minutes) into account
+    if dict_loc['type'] == "phone":
+        duration_to_crime_scene -= datetime.timedelta(minutes=2)
+
+    if available_time < duration_to_crime_scene:
+        # Innocenting the suspect
+        print(f"{suspect.name} : the suspect's last registered location is on " 
+              f"{time.strftime(time_format)}. It is "
+              f"{(available_time/datetime.timedelta(minutes=1)):.2f} minutes "
+               "apart from the crime date and time, and they would need "
+              f"{(duration_to_crime_scene.seconds)/60:.2f} minutes to get "
+              f"there. Therefore, {suspect.name} is innocent.")
+        suspect.is_suspect = False
